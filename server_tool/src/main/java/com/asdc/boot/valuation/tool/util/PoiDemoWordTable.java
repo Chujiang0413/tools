fa441b38e4fb221a21e0d79310643cab0b2393f2
chujiang <chujiang0413@163.com>
2019-10-11 16:07:59 +0800

    test

--- server_tool/src/main/java/com/asdc/boot/valuation/tool/util/PoiDemoWordTable.java
+++ server_tool/src/main/java/com/asdc/boot/valuation/tool/util/PoiDemoWordTable.java
@@ -1,32 +1,30 @@
-//package com.asdc.boot.valuation.tool.util;
-//
-//import org.apache.poi.xwpf.usermodel.XWPFDocument;
-//
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.io.FileOutputStream;
-//import java.io.InputStream;
-//
-//import static com.asdc.boot.valuation.tool.util.WordUtils.replaceAll;
-//
-//public class PoiDemoWordTable {
-//    public static void main(String[] args) throws Exception {
-//        final String returnurl = "d://poi/test.docx";
-//        final String templateurl = "D://poi/onepage.docx";
-//        InputStream is = new FileInputStream(new File(templateurl));
-//        XWPFDocument doc = new XWPFDocument(is);
-//        replaceAll(doc);
-//        //文件存在删除
-//        try {
-//            File file = new File(returnurl);
-//            if (file.exists()) {
-//                file.delete();
-//            }
-//            FileOutputStream fos = new FileOutputStream(returnurl);
-//            doc.write(fos);
-//            fos.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package com.asdc.boot.valuation.tool.util;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+
+public class PoiDemoWordTable {
+    public static void main(String[] args) throws Exception {
+        final String returnurl = "d://poi/test.docx";
+        final String templateurl = "D://poi/onepage.docx";
+        InputStream is = new FileInputStream(new File(templateurl));
+        XWPFDocument doc = new XWPFDocument(is);
+        //文件存在删除
+        try {
+            File file = new File(returnurl);
+            if (file.exists()) {
+                file.delete();
+            }
+            FileOutputStream fos = new FileOutputStream(returnurl);
+            doc.write(fos);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
